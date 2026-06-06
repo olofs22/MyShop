431436d2a04684f01e784ec752f5229dc65afd5b
olofs22 <olof.business.svensson@gmail.com>
2026-06-06 13:58:48 +0200

    added appdbcontext, repos, connectionstring

---
+++ backend/MyShop.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShop.Domain.Entities;
+
+namespace MyShop.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics" },
+                new Category { Id = 2, Name = "Books" }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Laptop", Price = 12000, CategoryId = 1 },
+                new Product { Id = 2, Name = "C# Handbook", Price = 350, CategoryId = 2 }
+            );
+
+        }
+    }
+}
